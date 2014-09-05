5799047861adc713cf8aafcd3b94dc72ebea2263
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-09-05 14:35:01 +0200

    Improved restctrl

--- src/main/scala/aj/slick/auth/controllers/RestCtrl.scala
+++ src/main/scala/aj/slick/auth/controllers/RestCtrl.scala
@@ -1,27 +1,45 @@
 package aj.slick.auth.controllers
 
-import org.json4s.jackson.Serialization._
-import org.json4s.JsonAST.JValue
-import org.json4s.{DefaultFormats, Extraction, Formats}
 import org.scalatra._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.Serialization._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
-trait RestCtrl extends ScalatraServlet {
+trait RestCtrl extends ScalatraServlet with JsonDSL with DoubleMode {
 
-  implicit val jsonFormats: Formats = DefaultFormats
+
+  implicit val formats = org.json4s.DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all
 
   implicit class JSONable(x: AnyRef) {
     def toJson: JValue  = Extraction.decompose(x)
   }
 
-  def getJson(transformers: RouteTransformer*)(action: => AnyRef): Route = {
-    get(transformers: _*) {
+  private def toPrettyJson(action: => Any) = action match {
+    // try to get some json out
+    case a@ActionResult(s, o: JValue, h) =>
       contentType = "application/json"
-      action match {
-        case v: JValue => writePretty(v)
-        case o => o
-      }
+      a.copy(body = pretty(render(o)))
+
+    case j:JValue =>
+      contentType = "application/json"
+      Ok(pretty(render(j)))
+
+    // pass the stuff through
+    case others => others
+  }
+
+  override def get(transformers: RouteTransformer*)(action: => Any): Route = {
+    super.get(transformers: _*) {
+      toPrettyJson(action)
     }
   }
 
+  override def post(transformers: RouteTransformer*)(action: => Any): Route = {
+    super.post(transformers: _*) {
+      toPrettyJson(action)
+    }
+  }
 }
+
 
